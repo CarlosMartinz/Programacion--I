304756cb76d6e56d4b97fa717dcf4c9b03b342c9
Carlos Manuel Abrego Martinez <usis044220@ugb.edu.sv>
2020-10-17 20:47:43 -0600

    DB

---
+++ NormalizacionDeBD/Calculadora/db_conexion.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data 'Importar las bibliotecas y clases necesarias para trabajar con ado.net
+Imports System.Data.SqlClient 'Biblioteca de clases para SQl server y ado.net
+Public Class db_conexion
+    Dim miConexion As New SqlConnection 'Conectarse a la BD
+    Dim miCommand As New SqlCommand 'Ejecutar consultas o sentencias SQL.
+    Dim miAdapter As New SqlDataAdapter 'Es un intermediario entre la fuente de datos y la aplicacion... como un puente 
+    Dim ds As New DataSet 'Representa una copia de la arquitectura (tablas, campos, indices, llaves, relaciones, datos, etc) de la BD en memoria
+    Public Sub New()
+        Dim cadenaConexion As String
+        cadenaConexion = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\db_escuela.mdf;Integrated Security=True"
+        miConexion.ConnectionString = cadenaConexion
+
+        miConexion.Open()
+
+        'parametrizacion()
+    End Sub
+End Class
