8e764c6927050683b2481dcbaf998f1665ca231a
Mauricio Enrique Vasquez Ramirez <usis007620@ugb.edu.sv>
2020-10-30 21:06:01 -0600

    Se trabajo y completo el Form de Edificio

---
+++ Calculadora/Calculadora/Busqueda/FormBusquedaEdificio.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormBusquedaEdificio
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblBuscarTipoHabit = New System.Windows.Forms.Label()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.grdBuscarEdificio = New System.Windows.Forms.DataGridView()
+        Me.btnSeleccionar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        CType(Me.grdBuscarEdificio, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblBuscarTipoHabit
+        '
+        Me.lblBuscarTipoHabit.AutoSize = True
+        Me.lblBuscarTipoHabit.Location = New System.Drawing.Point(12, 9)
+        Me.lblBuscarTipoHabit.Name = "lblBuscarTipoHabit"
+        Me.lblBuscarTipoHabit.Size = New System.Drawing.Size(40, 13)
+        Me.lblBuscarTipoHabit.TabIndex = 11
+        Me.lblBuscarTipoHabit.Text = "Buscar"
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtBuscar.Location = New System.Drawing.Point(57, 6)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(415, 20)
+        Me.txtBuscar.TabIndex = 10
+        '
+        'grdBuscarEdificio
+        '
+        Me.grdBuscarEdificio.AllowUserToAddRows = False
+        Me.grdBuscarEdificio.AllowUserToDeleteRows = False
+        Me.grdBuscarEdificio.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grdBuscarEdificio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdBuscarEdificio.Location = New System.Drawing.Point(12, 32)
+        Me.grdBuscarEdificio.Name = "grdBuscarEdificio"
+        Me.grdBuscarEdificio.ReadOnly = True
+        Me.grdBuscarEdificio.Size = New System.Drawing.Size(460, 284)
+        Me.grdBuscarEdificio.TabIndex = 9
+        '
+        'btnSeleccionar
+        '
+        Me.btnSeleccionar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSeleccionar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSeleccionar.Location = New System.Drawing.Point(247, 336)
+        Me.btnSeleccionar.Name = "btnSeleccionar"
+        Me.btnSeleccionar.Size = New System.Drawing.Size(118, 32)
+        Me.btnSeleccionar.TabIndex = 13
+        Me.btnSeleccionar.Text = "Seleccionar"
+        Me.btnSeleccionar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancelar.Location = New System.Drawing.Point(371, 336)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(101, 32)
+        Me.btnCancelar.TabIndex = 12
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'FormBusquedaEdificio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 389)
+        Me.Controls.Add(Me.btnSeleccionar)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.lblBuscarTipoHabit)
+        Me.Controls.Add(Me.txtBuscar)
+        Me.Controls.Add(Me.grdBuscarEdificio)
+        Me.Name = "FormBusquedaEdificio"
+        Me.Text = "FormBusquedaEdificio"
+        CType(Me.grdBuscarEdificio, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblBuscarTipoHabit As Label
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents grdBuscarEdificio As DataGridView
+    Friend WithEvents btnSeleccionar As Button
+    Friend WithEvents btnCancelar As Button
+End Class
--- Calculadora/Calculadora/Registro/FormEdificios.vb
+++ Calculadora/Calculadora/Registro/FormEdificios.vb
@@ -1,3 +1,120 @@
 ﻿Public Class FormEdificios
+    Dim objConexion As New db_conexion()
+    Dim dataTable As New DataTable
+    Dim posicion As Integer
+    Dim accion As String = "nuevo"
 
+    Private Sub FormEdificios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        obtenerDatosEdificio()
+        HabControles(True)
+    End Sub
+    Sub obtenerDatosEdificio()
+        dataTable = objConexion.obtenerDatosTablas().Tables("Edificio")
+        dataTable.PrimaryKey = New DataColumn() {dataTable.Columns("Edificio")}
+
+        mostrarDatosEdificio()
+    End Sub
+    Sub mostrarDatosEdificio()
+        If dataTable.Rows.Count > 0 Then
+            Me.Tag = dataTable.Rows(posicion).ItemArray(0).ToString() 'ID de tipo de habitacion 
+            txtNombreEdificio.Text = dataTable.Rows(posicion).ItemArray(0).ToString()
+
+            lblRegistroEdificio.Text = posicion + 1 & " de " & dataTable.Rows.Count
+        Else
+            txtNombreEdificio.Text = ""
+            MessageBox.Show("No hay registros que mostrar", "Registro de Productos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Sub HabControles(ByVal estado As Boolean)
+        grbDatos.Enabled = Not estado
+        grbNavegacion.Enabled = estado
+        btnEliminar.Enabled = estado
+        btnBuscar.Enabled = estado
+    End Sub
+
+    Private Sub btnPrimero_Click(sender As Object, e As EventArgs) Handles btnPrimero.Click
+        posicion = 0
+        mostrarDatosEdificio()
+    End Sub
+
+    Private Sub btnAnterior_Click(sender As Object, e As EventArgs) Handles btnAnterior.Click
+        If posicion > 0 Then
+            posicion -= 1
+            mostrarDatosEdificio()
+        Else
+            MessageBox.Show("Ya se encuentra en el primer registro", "Registro Edificio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub btnSiguiente_Click(sender As Object, e As EventArgs) Handles btnSiguiente.Click
+        If posicion < dataTable.Rows.Count - 1 Then
+            posicion += 1
+            mostrarDatosEdificio()
+        Else
+            MessageBox.Show("Ya se encuentra en el ultimo registro", "Registro Edificio", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub btnUltimo_Click(sender As Object, e As EventArgs) Handles btnUltimo.Click
+        posicion = dataTable.Rows.Count - 1
+        mostrarDatosEdificio()
+    End Sub
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        If btnNuevo.Text = "Nuevo" Then 'Nuevo
+            btnNuevo.Text = "Guardar"
+            btnModificar.Text = "Cancelar"
+            accion = "nuevo"
+
+            HabControles(False)
+            txtNombreEdificio.Text = ""
+        Else 'Guardar
+            Dim msg = objConexion.mantenimientoDatosEdificio(New String() {Me.Tag, txtNombreEdificio.Text}, accion)
+            If msg = "error" Then
+                MessageBox.Show("Error al intentar guardar el registro, por favor intente nuevamente.", "Registro de Edificio",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                obtenerDatosEdificio()
+                HabControles(True)
+                btnNuevo.Text = "Nuevo"
+                btnModificar.Text = "Modificar"
+            End If
+        End If
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        If btnModificar.Text = "Modificar" Then 'Modificar
+            btnNuevo.Text = "Guardar"
+            btnModificar.Text = "Cancelar"
+            accion = "actualizar"
+            HabControles(False)
+        Else 'Cancelar
+            obtenerDatosEdificio()
+
+            HabControles(True)
+            btnNuevo.Text = "Nuevo"
+            btnModificar.Text = "Modificar"
+        End If
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        If MessageBox.Show("Esta seguro de borrar Este Registro", "Registro de Edificio",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            objConexion.mantenimientoDatosEdificio(New String() {txtNombreEdificio.Text}, "eliminar")
+            If posicion > 0 Then
+                posicion -= 1 'Hemos borrado un registro
+            End If
+            obtenerDatosEdificio()
+        End If
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim objFormBuscarEdificio As New FormBusquedaEdificio()
+        objFormBuscarEdificio.ShowDialog()
+        If objFormBuscarEdificio._idEdificio <> "" Then
+            posicion = dataTable.Rows.IndexOf(dataTable.Rows.Find(objFormBuscarEdificio._idEdificio))
+            mostrarDatosEdificio()
+        End If
+    End Sub
 End Class
