bf3d52db468900c3b44b9e590ff27846a1123cad
Mauricio Enrique Vasquez Ramirez <usis007620@ugb.edu.sv>
2020-11-20 21:20:44 -0600

    Se trabajo en el Form Productos

--- Calculadora/Calculadora/Modulos/Registro/FormProductos.vb
+++ Calculadora/Calculadora/Modulos/Registro/FormProductos.vb
@@ -1,3 +1,36 @@
 ﻿Public Class FormProductos
+    Dim objConexion As New db_conexion
+    Dim dataTable As New DataTable
+    Dim Accion As String = "nuevo"
+    Dim Posicion As Integer = 0
 
+    Private Sub FormProductos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Sub ObtenerDatos()
+        dataTable = objConexion.obtenerDatosTablas().tables("Producto")
+        dataTable.PrimaryKey = New DataColumn() {dataTable.Columns("idProducto")}
+
+        cboCategoria.DataSource = objConexion.obtenerDatosTablas().Tables("Categoria").DefaultView()
+        cboCategoria.DisplayMember = "Categoria"
+        cboCategoria.ValueMember = "Categoria.idCategoria"
+
+        MostrarDatos()
+    End Sub
+
+    Sub MostrarDatos()
+        If dataTable.Rows.Count > 0 Then
+            Me.Tag = dataTable.Rows(Posicion).ItemArray(0).ToString()
+            cboCategoria.SelectedValue = dataTable.Rows(Posicion).ItemArray(1).ToString()
+            txtCodigo.Text = dataTable.Rows(Posicion).ItemArray(2).ToString()
+            txtDescripcion.Text = dataTable.Rows(Posicion).ItemArray(3).ToString()
+            txtPrecio.Text = dataTable.Rows(Posicion).ItemArray(4).ToString()
+
+            lblPosicion.Text = Posicion + 1 & " de " & dataTable.Rows.Count
+        Else
+            'limpiarDatosUsuarios()
+            MessageBox.Show("No hay registros que mostrar", "Registro Productos", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
 End Class
--- Calculadora/Calculadora/My Project/Resources.Designer.vb
+++ Calculadora/Calculadora/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Beer_Bottle_icon_icons_com_68782() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Beer_Bottle_icon-icons.com_68782", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Bodega() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Bodega", resourceCulture)
