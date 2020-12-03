a20bc995862f52963cd6188e1f14e9ffd048dd83
Mauricio Enrique Vasquez Ramirez <usis007620@ugb.edu.sv>
2020-12-02 20:51:20 -0600

    Se comenzo a trabajar en los reportes de lista de clientes y habitaciones

--- Calculadora/Calculadora/My Project/Resources.Designer.vb
+++ Calculadora/Calculadora/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Beer_Bottle_icon_icons1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Beer_Bottle_icon-icons1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Bodega() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Bodega", resourceCulture)
