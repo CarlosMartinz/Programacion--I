0816c7bd8a8c40fba8a92c59cf81c73d30c651d2
usis044220 <usis044220@ugb.edu.sv>
2020-12-09 21:11:11 -0600

    Se aplico el reporte

--- Calculadora/Calculadora/Modulos/Reportes/FormReporteListadoHabitacioens.Designer.vb
+++ Calculadora/Calculadora/Modulos/Reportes/FormReporteListadoHabitacioens.Designer.vb
@@ -23,13 +23,24 @@ Partial Class FormReporteListadoHabitacioens
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormReporteListadoHabitacioens))
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(800, 450)
+        Me.ReportViewer1.TabIndex = 0
         '
         'FormReporteListadoHabitacioens
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ReportViewer1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "FormReporteListadoHabitacioens"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -37,4 +48,6 @@ Partial Class FormReporteListadoHabitacioens
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
 End Class
--- Calculadora/Calculadora/Modulos/Reportes/FormReporteListadoHabitacioens.vb
+++ Calculadora/Calculadora/Modulos/Reportes/FormReporteListadoHabitacioens.vb
@@ -1,3 +1,6 @@
 ﻿Public Class FormReporteListadoHabitacioens
+    Private Sub FormReporteListadoHabitacioens_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        Me.ReportViewer1.RefreshReport()
+    End Sub
 End Class
